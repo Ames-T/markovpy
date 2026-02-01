f3bcf31048f34d6c312a7de557199f9e235f7fda
Ames <58957104+jeppeport@users.noreply.github.com>
2026-02-01 12:34:05 +0000

    Attempting to fix test VENV error in build path

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="markovpy",
     version="0.2.0",
     packages=find_packages(),
+    package_dir={"": "src"},
     install_requires=[
         "numpy>=1.26",  # Required for matrix operations
     ],
@@ -12,7 +13,7 @@ setup(
             "pytest>=7.4",  # For running tests
         ],
     },
-    python_requires=">=3.8",  # or >=3.7 if you want
+    python_requires=">=3.8",
     description="A Python library for discrete-time Markov chains",
     author="Ames T.",
     url="https://github.com/Ames-T/markovpy",
